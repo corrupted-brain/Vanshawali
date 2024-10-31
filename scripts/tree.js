d88fc2c622d7094a1c094fbb6c8d1ffe26defdcb
kailash0x01 <kailashbohara0x00@gmail.com>
2024-10-31 12:53:35 +0545

    Update tree.js

--- scripts/tree.js
+++ scripts/tree.js
@@ -5,227 +5,483 @@ var publicTree;
 var running = 1; // number of running asynchronous functions
 
 function parseTree(tree, replace) {
-    if (typeof replace !== 'undefined') {
-        replace.children = tree.children;
-        parseTree(tree);
-    } else if (tree.source) {
-        running++;
-        d3.json(tree.source, function(error, treeData) {
-            running--;
-            parseTree(treeData, tree);
-        });
-    } else if (tree.children) {
-        $(tree.children).each(function() {
-            parseTree(this);
-        });
-    }
+	if (typeof replace !== 'undefined') {
+		replace.children = tree.children;
+		parseTree(tree);
+	} else if (tree.source) {
+		running++;
+		d3.json(tree.source, function(error, treeData) {
+			running--;
+			parseTree(treeData, tree);
+		});
+	} else if (tree.children) {
+		$(tree.children).each(function() {
+			parseTree(this);
+		});
+	}
 }
 
 $.get('data/trans.yml').done(function(dataStr) {
-    console.log(dataStr);
-    const treeData = jsyaml.load(dataStr);
-    publicTree = treeData;
-    parseTree(publicTree);
-    running--;
+	console.log(dataStr);
+	const treeData = jsyaml.load(dataStr);
+	publicTree = treeData;
+	parseTree(publicTree);
+	running--;
 });
 
 function checkIfDone() {
-    if (running > 0) {
-        setTimeout(checkIfDone, 100);
-    } else {
-        drawTree(publicTree);
-    }
+	if (running > 0) {
+		setTimeout(checkIfDone, 100);
+	} else {
+		drawTree(publicTree);
+	}
 }
 checkIfDone();
 
 function drawTree(treeData) {
-    var vertical = false;
+	var vertical = false;
 
-    var totalNodes = 0;
-    var maxLabelLength = 0;
-    var panSpeed = 200;
-    var panBoundary = 20;
-    var i = 0;
-    var duration = 750;
-    var root;
-    var maxDepth = 0;
+	// Calculate total nodes, max label length
+	var totalNodes = 0;
+	var maxLabelLength = 0;
+	// panning variables
+	var panSpeed = 200;
+	var panBoundary = 20; // Within 20px from edges will pan when dragging.
+	// Misc. variables
+	var i = 0;
+	var duration = 750;
+	var root;
+	var maxDepth = 0;
 
-    var viewerWidth = $(document).width();
-    var viewerHeight = $(document).height();
+	// size of the diagram
+	var viewerWidth = $(document).width();
+	var viewerHeight = $(document).height();
 
-    var tree = d3.layout.tree().size([viewerHeight, viewerWidth]);
+	var tree = d3.layout.tree().size([viewerHeight, viewerWidth]);
 
-    var diagonal = d3.svg.diagonal().projection(function(d) {
-        return vertical ? [d.x, d.y] : [d.y, d.x];
-    });
+	// define a d3 diagonal projection for use by the node paths later on.
+	var diagonal = d3.svg.diagonal().projection(function(d) {
+		if (vertical) {
+			return [d.x, d.y];
+		} else {
+			return [d.y, d.x];
+		}
+	});
 
-    function englishName(d) {
-        return d['english-name'] ? d['english-name'] : d.name;
-    }
+	function englishName(d) {
+		return d['english-name'] ? d['english-name'] : d.name;
+	}
 
-    function visit(parent, visitFn, childrenFn) {
-        if (!parent) return;
-        visitFn(parent);
-        var children = childrenFn(parent);
-        if (children) {
-            children.forEach(function(child) {
-                visit(child, visitFn, childrenFn);
-            });
-        }
-    }
+	// A recursive helper function for performing some setup by walking through all nodes
+	function visit(parent, visitFn, childrenFn) {
+		if (!parent) {
+			return;
+		}
+		visitFn(parent);
+		var children = childrenFn(parent);
+		if (children) {
+			var count = children.length;
+			for (var i = 0; i < count; i++) {
+				visit(children[i], visitFn, childrenFn);
+			}
+		}
+	}
 
-    visit(treeData, function(d) {
-        totalNodes++;
-        maxLabelLength = Math.max(englishName(d).length, maxLabelLength);
-    }, function(d) {
-        return d.children && d.children.length > 0 ? d.children : null;
-    });
+	// Call visit function to establish maxLabelLength
+	visit(
+		treeData,
+		function(d) {
+			totalNodes++;
+			maxLabelLength = Math.max(englishName(d).length, maxLabelLength);
+		},
+		function(d) {
+			return d.children && d.children.length > 0 ? d.children : null;
+		}
+	);
 
-    function pan(domNode, direction) {
-        var speed = panSpeed;
-        if (panTimer) {
-            clearTimeout(panTimer);
-            var translateCoords = d3.transform(svgGroup.attr('transform'));
-            var translateX = direction == 'left' ? translateCoords.translate[0] + speed : translateCoords.translate[0] - speed;
-            var translateY = direction == 'up' ? translateCoords.translate[1] + speed : translateCoords.translate[1] - speed;
-            var scale = zoomListener.scale();
-            svgGroup.transition().attr('transform', 'translate(' + translateX + ',' + translateY + ')scale(' + scale + ')');
-            zoomListener.translate([translateX, translateY]);
-            panTimer = setTimeout(function() {
-                pan(domNode, speed, direction);
-            }, 50);
-        }
-    }
+	// TODO: Pan function, can be better implemented.
+	function pan(domNode, direction) {
+		var speed = panSpeed;
+		if (panTimer) {
+			clearTimeout(panTimer);
+			translateCoords = d3.transform(svgGroup.attr('transform'));
+			if (direction == 'left' || direction == 'right') {
+				translateX =
+					direction == 'left' ? translateCoords.translate[0] + speed : translateCoords.translate[0] - speed;
+				translateY = translateCoords.translate[1];
+			} else if (direction == 'up' || direction == 'down') {
+				translateX = translateCoords.translate[0];
+				translateY =
+					direction == 'up' ? translateCoords.translate[1] + speed : translateCoords.translate[1] - speed;
+			}
+			scaleX = translateCoords.scale[0];
+			scaleY = translateCoords.scale[1];
+			scale = zoomListener.scale();
+			svgGroup
+				.transition()
+				.attr('transform', 'translate(' + translateX + ',' + translateY + ')scale(' + scale + ')');
+			d3
+				.select(domNode)
+				.select('g.node')
+				.attr('transform', 'translate(' + translateX + ',' + translateY + ')');
+			zoomListener.scale(zoomListener.scale());
+			zoomListener.translate([translateX, translateY]);
+			panTimer = setTimeout(function() {
+				pan(domNode, speed, direction);
+			}, 50);
+		}
+	}
 
-    function zoom() {
-        svgGroup.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
-    }
+	// Define the zoom function for the zoomable tree
+	function zoom() {
+		svgGroup.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
+	}
 
-    var zoomListener = d3.behavior
-        .zoom()
-        .scaleExtent([0.1, 3]) // Set minimum zoom level to 0.1 for maximum zoom-out
-        .on('zoom', zoom);
+	// define the zoomListener which calls the zoom function on the "zoom" event constrained within the scaleExtents
+	var zoomListener = d3.behavior
+		.zoom()
+		.scaleExtent([0.1, 3])
+		.on('zoom', zoom);
 
-    var baseSvg = d3
-        .select('#tree-container')
-        .append('svg')
-        .attr('width', viewerWidth)
-        .attr('height', viewerHeight)
-        .attr('class', 'overlay')
-        .call(zoomListener);
+	// define the baseSvg, attaching a class for styling and the zoomListener
+	var baseSvg = d3
+		.select('#tree-container')
+		.append('svg')
+		.attr('width', viewerWidth)
+		.attr('height', viewerHeight)
+		.attr('class', 'overlay')
+		.call(zoomListener);
 
-    function centerNode(source) {
-        var scale = 0.1; // Set minimum zoom level
-        var x = -source.y0;
-        var y = -source.x0;
-        x = x * scale + viewerWidth / 2;
-        y = y * scale + viewerHeight / 2;
-        d3.select('g')
-            .transition()
-            .duration(duration)
-            .attr('transform', 'translate(' + x + ',' + y + ')scale(' + scale + ')');
-        zoomListener.translate([x, y]).scale(scale);
-    }
+	// Helper functions for collapsing and expanding nodes.
 
-    function toggleChildren(d) {
-        if (d.children) {
-            d._children = d.children;
-            d.children = null;
-        } else if (d._children) {
-            d.children = d._children;
-            d._children = null;
-        }
-        return d;
-    }
+	function collapse(d) {
+		if (d.children) {
+			d._children = d.children;
+			d._children.forEach(collapse);
+			d.children = null;
+		}
+	}
 
-    function click(d) {
-        if (d3.event.defaultPrevented) return;
-        d = toggleChildren(d);
-        update(d);
-        centerNode(d);
-    }
+	function expand(d) {
+		if (d._children) {
+			d.children = d._children;
+			d.children.forEach(expand);
+			d._children = null;
+		}
+	}
 
-    function update(source) {
-        var levelWidth = [1];
-        var childCount = function(level, n) {
-            if (n.children && n.children.length > 0) {
-                if (levelWidth.length <= level + 1) levelWidth.push(0);
-                levelWidth[level + 1] += n.children.length;
-                n.children.forEach(function(d) {
-                    childCount(level + 1, d);
-                });
-            }
-        };
-        childCount(0, root);
-        var newHeight = d3.max(levelWidth) * 70;
-        tree = tree.size([newHeight, viewerWidth]);
+	var overCircle = function(d) {
+		selectedNode = d;
+		updateTempConnector();
+	};
+	var outCircle = function(d) {
+		selectedNode = null;
+		updateTempConnector();
+	};
 
-        var nodes = tree.nodes(root).reverse(),
-            links = tree.links(nodes);
+	function centerNode(source) {
+		scale = zoomListener.scale();
+		x = -source.y0;
+		y = -source.x0;
+		x = x * scale + viewerWidth / 2;
+		y = y * scale + viewerHeight / 2;
+		d3
+			.select('g')
+			.transition()
+			.duration(duration)
+			.attr('transform', 'translate(' + x + ',' + y + ')scale(' + scale + ')');
+		//zoomListener.scale(scale);
+		zoomListener.translate([x, y]);
+	}
 
-        nodes.forEach(function(d) {
-            if (d.depth > maxDepth) maxDepth = d.depth;
-            d.y = d.depth * (maxLabelLength * (vertical ? 5 : 8));
-        });
+	// Toggle children function
+	function toggleChildren(d) {
+		if (d.children) {
+			d._children = d.children;
+			d.children = null;
+		} else if (d._children) {
+			d.children = d._children;
+			d._children = null;
+		}
+		return d;
+	}
 
-        var node = svgGroup.selectAll('g.node').data(nodes, function(d) {
-            return d.id || (d.id = ++i);
-        });
+	// Toggle children on click.
+	function click(d) {
+		if (d3.event.defaultPrevented) {
+			return;
+		} // click suppressed
+		d = toggleChildren(d);
+		update(d);
+		centerNode(d);
+	}
 
-        var nodeEnter = node.enter().append('g').attr('class', 'node').attr('transform', function(d) {
-            return vertical ? 'translate(' + source.x0 + ',' + source.y0 + ')' : 'translate(' + source.y0 + ',' + source.x0 + ')';
-        }).on('click', click);
+	function update(source) {
+		// Compute the new height, function counts total children of root node and sets tree height accordingly.
+		// This prevents the layout looking squashed when new nodes are made visible or looking sparse when nodes are removed
+		// This makes the layout more consistent.
+		var levelWidth = [1];
+		var childCount = function(level, n) {
+			if (n.children && n.children.length > 0) {
+				if (levelWidth.length <= level + 1) {
+					levelWidth.push(0);
+				}
 
-        nodeEnter.append('circle').attr('class', 'nodeCircle').attr('r', 10).style('fill', function(d) {
-            return d._children ? 'lightsteelblue' : '#fff';
-        });
+				levelWidth[level + 1] += n.children.length;
+				n.children.forEach(function(d) {
+					childCount(level + 1, d);
+				});
+			}
+		};
+		childCount(0, root);
+		var newHeight = d3.max(levelWidth) * 70; // 70 pixels per line
+		tree = tree.size([newHeight, viewerWidth]);
 
-        nodeEnter.append('text').attr('x', -20).attr('dy', '.35em').attr('class', 'nodeText').attr('text-anchor', 'end').text(function(d) {
-            return englishName(d);
-        }).style('fill-opacity', 0);
+		// Compute the new tree layout.
+		var nodes = tree.nodes(root).reverse(),
+			links = tree.links(nodes);
 
-        var nodeUpdate = node.transition().duration(duration).attr('transform', function(d) {
-            return vertical ? 'translate(' + d.x + ',' + d.y + ')' : 'translate(' + d.y + ',' + d.x + ')';
-        });
+		// Set widths between levels based on maxLabelLength.
+		nodes.forEach(function(d) {
+			if (d.depth > maxDepth) {
+				maxDepth = d.depth;
+			}
+			if (vertical) {
+				d.y = d.depth * (maxLabelLength * 5);
+			} else {
+				d.y = d.depth * (maxLabelLength * 8);
+			}
+		});
 
-        nodeUpdate.select('text').style('fill-opacity', 1);
+		// Update the nodes…
+		node = svgGroup.selectAll('g.node').data(nodes, function(d) {
+			return d.id || (d.id = ++i);
+		});
 
-        var nodeExit = node.exit().transition().duration(duration).attr('transform', function(d) {
-            return vertical ? 'translate(' + source.x + ',' + source.y + ')' : 'translate(' + source.y + ',' + source.x + ')';
-        }).remove();
+		// Enter any new nodes at the parent's previous position.
+		var nodeEnter = node
+			.enter()
+			.append('g')
+			.attr('class', 'node')
+			.attr('transform', function(d) {
+				if (vertical) {
+					return 'translate(' + source.x0 + ',' + source.y0 + ')';
+				} else {
+					return 'translate(' + source.y0 + ',' + source.x0 + ')';
+				}
+			})
+			.on('click', click);
 
-        nodeExit.select('circle').attr('r', 0);
-        nodeExit.select('text').style('fill-opacity', 0);
+		nodeEnter
+			.append('circle')
+			.attr('class', 'nodeCircle')
+			.attr('r', 0)
+			.style('fill', function(d) {
+				return d._children ? 'lightsteelblue' : '#fff';
+			});
 
-        var link = svgGroup.selectAll('path.link').data(links, function(d) {
-            return d.target.id;
-        });
+		if (vertical) {
+			nodeEnter
+				.append('text')
+				.attr('y', function(d) {
+					return d.children || d._children ? -18 : 18;
+				})
+				.attr('dy', '.35em')
+				.attr('text-anchor', 'middle')
+				.text(function(d) {
+					return englishName(d);
+				})
+				.style('fill-opacity', 1);
+		} else {
+			nodeEnter
+				.append('text')
+				.attr('x', function(d) {
+					return -20;
+				})
+				.attr('dy', '.35em')
+				.attr('class', 'nodeText')
+				.attr('text-anchor', function(d) {
+					return 'end';
+				})
+				.text(function(d) {
+					return englishName(d);
+				})
+				.style('fill-opacity', 0);
+		}
 
-        link.enter().insert('path', 'g').attr('class', 'link').style('stroke-width', function(d) {
-            return 3 * (maxDepth - d.source.depth) + 'px';
-        }).attr('d', function(d) {
-            var o = {x: source.x0, y: source.y0};
-            return diagonal({source: o, target: o});
-        });
+		// append an image if one exists
+		nodeEnter
+			.append('image')
+			.attr('title', '')
+			.attr('xlink:href', '')
+			.attr('x', -13)
+			.attr('y', -13)
+			.attr('width', 25)
+			.attr('height', 25);
 
-        link.transition().duration(duration).attr('d', diagonal);
-        link.exit().transition().duration(duration).attr('d', function(d) {
-            var o = {x: source.x, y: source.y};
-            return diagonal({source: o, target: o});
-        }).remove();
+		// phantom node to give us mouseover in a radius around it
+		nodeEnter
+			.append('circle')
+			.attr('class', 'ghostCircle')
+			.attr('r', 30)
+			.attr('opacity', 0.2) // change this to zero to hide the target area
+			.style('fill', 'red')
+			.attr('pointer-events', 'mouseover')
+			.on('mouseover', function(node) {
+				overCircle(node);
+			})
+			.on('mouseout', function(node) {
+				outCircle(node);
+			});
 
-        nodes.forEach(function(d) {
-            d.x0 = d.x;
-            d.y0 = d.y;
-        });
-    }
+		node.select('image').attr('xlink:href', function(d) {
+			if (d.image) {
+				return d.image;
+			} else if (d.isFemale) {
+				return 'images/placeholder-female.png';
+			} else {
+				return 'images/placeholder.png';
+			}
+		});
+		node.select('image').attr('title', function(d) {
+			return '<strong>' + englishName(d) + '</strong>. ' + (d.bio ? d.bio : '');
+		});
 
-    var svgGroup = baseSvg.append('g');
+		// Update the text to reflect whether node has children or not.
+		node
+			.select('text')
+			.attr('x', function(d) {
+				//return d.children || d._children ? -10 : 10;
+				return -20;
+			})
+			.attr('text-anchor', function(d) {
+				//return d.children || d._children ? "end" : "start";
+				return 'end';
+			})
+			.text(function(d) {
+				return englishName(d);
+			});
 
-    root = treeData;
-    root.x0 = viewerHeight / 2;
-    root.y0 = 0;
+		// Change the circle fill depending on whether it has children and is collapsed
+		node
+			.select('circle.nodeCircle')
+			.attr('r', 10)
+			.style('fill', function(d) {
+				return d._children ? 'lightsteelblue' : '#fff';
+			});
 
-    update(root);
-    centerNode(root);
+		// Transition nodes to their new position.
+		var nodeUpdate = node
+			.transition()
+			.duration(duration)
+			.attr('transform', function(d) {
+				if (vertical) {
+					return 'translate(' + d.x + ',' + d.y + ')';
+				} else {
+					return 'translate(' + d.y + ',' + d.x + ')';
+				}
+			});
+
+		// Fade the text in
+		nodeUpdate.select('text').style('fill-opacity', 1);
+
+		// Transition exiting nodes to the parent's new position.
+		var nodeExit = node
+			.exit()
+			.transition()
+			.duration(duration)
+			.attr('transform', function(d) {
+				if (vertical) {
+					return 'translate(' + source.x + ',' + source.y + ')';
+				} else {
+					return 'translate(' + source.y + ',' + source.x + ')';
+				}
+			})
+			.remove();
+
+		nodeExit.select('circle').attr('r', 0);
+
+		nodeExit.select('text').style('fill-opacity', 0);
+
+		// Update the links…
+		var link = svgGroup.selectAll('path.link').data(links, function(d) {
+			return d.target.id;
+		});
+
+		// Enter any new links at the parent's previous position.
+		link
+			.enter()
+			.insert('path', 'g')
+			.attr('class', 'link')
+			.style('stroke-width', function(d) {
+				return 3 * (maxDepth - d.source.depth) + 'px';
+			})
+			.attr('d', function(d) {
+				var o = {
+					x: source.x0,
+					y: source.y0,
+				};
+				return diagonal({
+					source: o,
+					target: o,
+				});
+			});
+
+		// Transition links to their new position.
+		link
+			.transition()
+			.duration(duration)
+			.attr('d', diagonal);
+
+		// Transition exiting nodes to the parent's new position.
+		link
+			.exit()
+			.transition()
+			.duration(duration)
+			.attr('d', function(d) {
+				var o = {
+					x: source.x,
+					y: source.y,
+				};
+				return diagonal({
+					source: o,
+					target: o,
+				});
+			})
+			.remove();
+
+		// Stash the old positions for transition.
+		nodes.forEach(function(d) {
+			d.x0 = d.x;
+			d.y0 = d.y;
+		});
+	}
+
+	// Append a group which holds all nodes and which the zoom Listener can act upon.
+	var svgGroup = baseSvg.append('g');
+
+	// Define the root
+	root = treeData;
+	root.x0 = viewerHeight / 2;
+	root.y0 = 0;
+
+	// Layout the tree initially and center on the root node.
+	update(root);
+	centerNode(root);
+
+	// Show biography and picture on hover
+	$('body').hoverIntent({
+		over: function() {
+			var bio = $(this).attr('title');
+			var img = $(this).attr('href');
+			$('#bio')
+				.html("<img src='" + $(this).attr('href') + "'>" + bio)
+				.addClass('has-image')
+				.fadeIn('fast');
+		},
+		out: function() {
+			$('#bio').fadeOut('fast');
+		},
+		selector: '.node image',
+	});
 }
